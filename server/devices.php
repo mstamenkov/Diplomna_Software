074b157125f40e561919c16b4f6b6191886666b9
Martin Stamenkov <bul2013@abv.bg>
2020-01-31 13:51:21 +0200

    updated sql script

--- server/devices.php
+++ server/devices.php
@@ -3,9 +3,17 @@ session_start();
     if (!isset($_SESSION['user_id'])) header('Location: ./login.html');
 
     $con = new mysqli("127.0.0.1", "root", "root", "ALARM_SYSTEM");
-    $con = new mysqli("127.0.0.1", "root", "root", "ALARM_SYSTEM");
-    $stmt = $con->prepare("SELECT * FROM modules WHERE id = ?");
+    $stmt = $con->prepare("SELECT * FROM modules WHERE userId = ?");
     $stmt->bind_param('i', $_SESSION['user_id']);
     $stmt->execute();
     $result = $stmt->get_result();
-    $modules = $result->fetch_object();
+    //echo $modules->id;
+    echo "<table>"; // start a table tag in the HTML
+
+    while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
+        echo "<tr><td>" . $row['id'] . "</td></tr>";  //$row['index'] the index here is a field name
+
+    }
+
+    echo "</table>";
+        $result->free();
